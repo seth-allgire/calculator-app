ecdca06c688dbb24fc1dbbfdb730ae6e8e2b8da2
Seth Allgire <seth.allgire@gmail.com>
2021-09-18 14:13:27 -0500

    adds eventListener for all buttons

--- calculator-app.js
+++ calculator-app.js
@@ -1,15 +1,67 @@
-const numberButtons = document.querySelectorAll(".number");
-const operationButtons = document.querySelectorAll(".operator");
-// const display = document.querySelector("#display");
+const calculator = {
+  displayValue: "0",
+  operand: null,
+  waitingForSecondOperand: false,
+  operator: null,
+};
 
-// numberButtons.addEventListener("click", updateDisplay);
+function updateDisplay() {
+  const display = document.querySelector("#operand");
+  display.value = calculator.displayValue;
+}
+updateDisplay();
 
-numberButtons.forEach(() => {
-  addEventListener("click", updateDisplay);
+const buttons = document.querySelector(".calculator-buttons");
+buttons.addEventListener("click", (e) => {
+  const target = e.target;
+
+  if (target.classList.contains("operation")) {
+    console.log("operation", target.value);
+    return;
+  }
+  if (target.classList.contains("clear-entry")) {
+    console.log("clear-entry", target.value);
+    return;
+  }
+  if (target.classList.contains("clear-all")) {
+    console.log("clear-all", target.value);
+    return;
+  }
+  if (target.classList.contains("decimal-point")) {
+    console.log("decimal", target.value);
+    return;
+  }
+  // if (target.classList.contains("number")) {
+  console.log("number", target.value);
+  // }
 });
 
-function updateDisplay(e) {
-  const display = e.target.value;
-  document.getElementById("display").innerText += display;
-  console.log(e.target.value);
-}
+// const numberButtons = document.querySelectorAll(".number");
+// const operatorButtons = document.querySelectorAll(".operator");
+
+// const output = document.querySelector("#operand");
+// let screenDisplay = output.innerText;
+
+// numberButtons.forEach((button) => {
+//   addEventListener("click", displayNumbers);
+// });
+
+// !!!!!!THIS WORKED ONCE UPON A TIME!!! BUT LOCKED IN TO NUMBERS ONLY
+// NEED TO CATCH ALL THE CLICKS ON ALL THE BUTTONS!!!!!!
+// function displayNumbers(e) {
+//   const display = e.target.value;
+//   document.querySelector("#operand").innerText += display;
+//   console.log(e.target.value);
+// }
+
+// operatorButtons.forEach((button) => {
+//   addEventListener("click", displayOperator);
+// });
+
+// function displayOperator(e) {}
+// }
+
+// function updateNumbers() {
+//   if (screen.innerText === "0") {
+//   }
+// }
